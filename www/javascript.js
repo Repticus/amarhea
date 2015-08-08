0367792ef9e739db0f4d483953d04551a5535710
Repticus <martin.bubik@flashlink.cz>
2015-08-08 21:24:37 +0200

    solved page height bug

--- www/javascript.js
+++ www/javascript.js
@@ -1,2 +1,8 @@
 $(function () {
+	setTimeout(setPageHeight, 50);
+
+	function setPageHeight() {
+		height = (Math.ceil(($("#page").height() - 585 - 509) / 98)) * 98 + 585 + 509;
+		$("#page").height(height);
+	}
 });
