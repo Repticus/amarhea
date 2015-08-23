4482a472221afd48fd4f3a60e5cec88896a0964f
Repticus <martin.bubik@flashlink.cz>
2015-08-23 15:51:10 +0200

    set up gallery in Kouzelne amulety

--- app/presenters/WebPresenter.php
+++ app/presenters/WebPresenter.php
@@ -2,12 +2,32 @@
 
 namespace App\Presenters;
 
-use Nette;
+use Nette,
+	 Nette\Utils\Finder;
 
 class WebPresenter extends Nette\Application\UI\Presenter {
 
 	public function renderDefault() {
 
+	}
+
+	public function actionKouzelneAmulety() {
+		$this->template->amulets = $this->context->parameters['amulets'];
+	}
+
+	public function getImages($gallery) {
+		try {
+			$images = array();
+			foreach (Finder::findFiles("*.jpg")->in("amulety/" . $gallery) as $file) {
+				$image['path'] = "amulety/$gallery/" . $file->getFilename();
+				$exif = exif_read_data($image['path']);
+				$image['title'] = isset($exif['ImageDescription']) ? $exif['ImageDescription'] : NULL;
+				$images[] = $image;
+			}
+			return $images;
+		} catch (Exception $e) {
+			return array();
+		}
 	}
 
 }
