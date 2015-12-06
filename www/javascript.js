febb84194c0275ccd5e9ce548f3d307d9cf593b3
Repticus <martin.bubik@flashlink.cz>
2015-12-06 16:35:55 +0100

    redesigned layout, gem animation

--- www/javascript.js
+++ www/javascript.js
@@ -1,8 +1,20 @@
 $(function () {
 	setTimeout(setPageHeight, 50);
+	setTimeout(showGem, 1000);
 
 	function setPageHeight() {
 		height = (Math.ceil(($("#page").height() - 585 - 509) / 98)) * 98 + 585 + 509;
 		$("#page").height(height);
 	}
+
+	function showGem() {
+		$("#gem")
+				  .fadeToggle(300)
+				  .fadeToggle(600)
+				  .fadeToggle(300)
+				  .fadeToggle(2000, function () {
+					  setTimeout(showGem, 1000);
+				  });
+	}
+
 });
