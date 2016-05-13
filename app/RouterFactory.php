43160202fda73057bd1170d7ba9087aba515b819
Repticus <martin.bubik@flashlink.cz>
2016-05-14 01:11:00 +0200

    updated menu renamed section KurzAstrologie to Astro-poradenstvi renamed section Poradenstvi a terapie to Energie-Kranioterapie

--- app/RouterFactory.php
+++ app/RouterFactory.php
@@ -14,6 +14,8 @@ class RouterFactory {
 	public static function createRouter() {
 		$router = new RouteList();
 		$router[] = new Route('kurzy', 'Web:kurzyAstroKonstelace', Route::ONE_WAY);
+		$router[] = new Route('poradenstvi-terapie', 'Web:energieKranioterapie', Route::ONE_WAY);
+		$router[] = new Route('kurz-astrologie', 'Web:astroPoradenstvi', Route::ONE_WAY);
 		$router[] = new Route('[<action>]', 'Web:amarhea');
 		return $router;
 	}
